6e411910165e79096ba3670bb5a6c4711e340661
Teemu Murtola <teemu.murtola@gmail.com>
2016-09-15 21:54:26 +0300

    Rewrite build system for the main library
    - Use object libraries for building parts that may need to be bundled to
      other libraries, and use various per-target properties that allow
      propagating, e.g., include directories and required compile
      definitions to using code automatically.
    - Use install(EXPORT) for creating the package configuration file, so
      that it actually works with all combinations.  That also automatically
      exports the INTERFACE_* properties so that include directories will
      get automatically managed on the using side.
    - Use the CMake-recommended find_dependency() for finding zlib for using
      applications automatically when required.
    - Fix some warnings from non-existent link directories and make static
      libraries also go to lib/.
    - Add a test project for testing linking against tng_io.
    
    Change-Id: I76c2ac305580920db4301461854775bf40ba0c53

--- BuildTNG.cmake
+++ BuildTNG.cmake
@@ -1,5 +1,8 @@
 set(TNG_ROOT_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR})
 file(RELATIVE_PATH TNG_ROOT_BINARY_DIR ${CMAKE_SOURCE_DIR} ${TNG_ROOT_SOURCE_DIR})
+if ("${TNG_ROOT_BINARY_DIR}" MATCHES "^\.\.")
+    set(TNG_ROOT_BINARY_DIR tng)
+endif()
 set(TNG_ROOT_BINARY_DIR ${CMAKE_BINARY_DIR}/${TNG_ROOT_BINARY_DIR})
 
 function (TNG_GENERATE_VERSION_H)
@@ -15,51 +18,86 @@ function (TNG_GENERATE_VERSION_H)
     set(TNG_IO_VERSION ${TNG_IO_VERSION} PARENT_SCOPE)
 endfunction()
 
-tng_generate_version_h()
-
 include(TestBigEndian)
 test_big_endian(TNG_INTEGER_BIG_ENDIAN)
 include(CheckIncludeFile)
 check_include_file(inttypes.h TNG_HAVE_INTTYPES_H)
+include(CMakeParseArguments)
 
-# Get a list of source files for compiling TNG, and perhaps its zlib dependency
-#
-# Parameters:
-#   BUILD_OWN_ZLIB   (input)  Boolean value whether to build zlib internally
-#   TNG_SOURCELIST   (output) Name of variable into which to store a list of source
-#                             files for compling TNG (plus perhaps zlib)
-#   TNG_COMPILEDEFS  (output) Name of variable into which to store the set of required defines
-#
-macro(TNG_GET_SOURCE_LIST BUILD_OWN_ZLIB TNG_SOURCELIST TNG_COMPILEDEFS)
-    include_directories(BEFORE ${TNG_ROOT_SOURCE_DIR}/include)
-    include_directories(BEFORE ${TNG_ROOT_BINARY_DIR}/include)
-    set(_tng_compression_sources bwlzh.c bwt.c coder.c dict.c fixpoint.c huffman.c huffmem.c lz77.c merge_sort.c mtf.c rle.c tng_compress.c vals16.c warnmalloc.c widemuldiv.c xtc2.c xtc3.c)
+function(add_tng_io_library NAME)
+    tng_generate_version_h()
+
+    set(_tng_compression_sources
+        bwlzh.c bwt.c coder.c dict.c fixpoint.c huffman.c huffmem.c
+        lz77.c merge_sort.c mtf.c rle.c tng_compress.c vals16.c
+        warnmalloc.c widemuldiv.c xtc2.c xtc3.c)
     set(_tng_io_sources tng_io.c md5.c)
-    set(${TNG_SOURCELIST})
-    set(${TNG_COMPILEDEFS})
+    set(_sources)
     foreach(_file ${_tng_compression_sources})
-        list(APPEND ${TNG_SOURCELIST} ${TNG_ROOT_SOURCE_DIR}/src/compression/${_file})
+        list(APPEND _sources ${TNG_ROOT_SOURCE_DIR}/src/compression/${_file})
     endforeach()
     foreach(_file ${_tng_io_sources})
-        list(APPEND ${TNG_SOURCELIST} ${TNG_ROOT_SOURCE_DIR}/src/lib/${_file})
+        list(APPEND _sources ${TNG_ROOT_SOURCE_DIR}/src/lib/${_file})
     endforeach()
     if(TNG_BUILD_FORTRAN)
-      list(APPEND ${TNG_SOURCELIST} ${TNG_ROOT_SOURCE_DIR}/src/lib/tng_io_fortran.c)
+        list(APPEND _sources ${TNG_ROOT_SOURCE_DIR}/src/lib/tng_io_fortran.c)
     endif()
-    if(BUILD_OWN_ZLIB)
+
+    set(_options OBJECT OWN_ZLIB)
+    cmake_parse_arguments(ARG "${_options}" "" "" ${ARGN})
+
+    set(_build_target ${NAME})
+    set(_link_type PRIVATE)
+    if (ARG_OBJECT)
+        set(_build_target tng_io_obj)
+        set(_link_type INTERFACE)
+        add_library(${_build_target} OBJECT ${_sources})
+        # PIC is only on by default for SHARED libraries, but in case the
+        # object library is going to get used in such a library, the objects
+        # should be compiled with PIC as well.
+        if (BUILD_SHARED_LIBS)
+            set_target_properties(${_build_target} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+        endif()
+        add_library(${NAME} INTERFACE)
+        target_sources(${NAME} INTERFACE $<TARGET_OBJECTS:tng_io_obj>)
+    else()
+        add_library(${NAME} ${_sources})
+        set_target_properties(${NAME} PROPERTIES
+                              VERSION ${TNG_IO_VERSION}
+                              SOVERSION ${TNG_MAJOR_VERSION})
+        target_include_directories(${NAME} INTERFACE $<INSTALL_INTERFACE:include>)
+    endif()
+    target_include_directories(${_build_target} PRIVATE
+                               $<BUILD_INTERFACE:${TNG_ROOT_SOURCE_DIR}/include>
+                               $<BUILD_INTERFACE:${TNG_ROOT_BINARY_DIR}/include>)
+    target_include_directories(${NAME} INTERFACE
+                               $<BUILD_INTERFACE:${TNG_ROOT_SOURCE_DIR}/include>
+                               $<BUILD_INTERFACE:${TNG_ROOT_BINARY_DIR}/include>)
+
+    if (UNIX)
+        target_link_libraries(${NAME} ${_link_type} m)
+    endif()
+
+    if (ARG_OWN_ZLIB)
+        set(_zlib_dir ${TNG_ROOT_SOURCE_DIR}/external/zlib)
+        set(_zlib_sources)
         # Add minimal necessary number of TNG source files
         foreach(_file adler32.c compress.c crc32.c deflate.c inffast.c inflate.c inftrees.c trees.c uncompr.c zutil.c)
-            list(APPEND ${TNG_SOURCELIST} ${TNG_ROOT_SOURCE_DIR}/external/zlib/${_file})
+            list(APPEND _zlib_sources ${_zlib_dir}/${_file})
         endforeach()
-        include_directories(BEFORE ${TNG_ROOT_SOURCE_DIR}/external/zlib)
+        add_library(tng_io_zlib OBJECT ${_zlib_sources})
+        if (BUILD_SHARED_LIBS)
+            set_target_properties(tng_io_zlib PROPERTIES POSITION_INDEPENDENT_CODE ON)
+        endif()
+        target_include_directories(tng_io_zlib PUBLIC ${_zlib_dir})
+        target_include_directories(${_build_target} PRIVATE ${_zlib_dir})
+        target_sources(${NAME} ${_link_type} $<TARGET_OBJECTS:tng_io_zlib>)
+    else()
+        target_link_libraries(${NAME} ${_link_type} ZLIB::ZLIB)
     endif()
-    if (TNG_HAVE_INTTYPES_H)
-        list(APPEND ${TNG_COMPILEDEFS} USE_STD_INTTYPES_H)
-    endif()
-endmacro()
 
-macro(TNG_SET_SOURCE_PROPERTIES)
     if (TNG_HAVE_INTTYPES_H)
+        target_compile_definitions(${NAME} INTERFACE USE_STD_INTTYPES_H)
         set_property(SOURCE ${TNG_ROOT_SOURCE_DIR}/src/lib/tng_io.c
                      APPEND PROPERTY COMPILE_DEFINITIONS USE_STD_INTTYPES_H)
     endif()
@@ -67,4 +105,4 @@ macro(TNG_SET_SOURCE_PROPERTIES)
         set_property(SOURCE ${TNG_ROOT_SOURCE_DIR}/src/lib/md5.c
                      APPEND PROPERTY COMPILE_DEFINITIONS TNG_INTEGER_BIG_ENDIAN)
     endif()
-endmacro()
+endfunction()
